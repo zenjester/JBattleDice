77a62d9398bea9aa88d8dbda5fb39bf085f2b6a5
zenjester <zenjester@gmail.com>
2015-01-31 13:28:24 +0000

    first commit

---
+++ JBattleDice/src/jbattledice/dieRoll.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * This is a new header
+ */
+package jbattledice;
+
+import java.util.Random;
+
+/**
+ *
+ * @author andrew
+ */
+public class dieRoll {
+   
+    private int roll;
+    private final Random rand;
+
+    public dieRoll() {
+        this.rand = new Random();
+    }
+    
+    public int rollResult(int value)
+    {
+       roll = rand.nextInt(value)+1;
+       return roll;
+    }
+}
---
+++ JBattleDice/test/jbattledice/dieRollTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbattledice;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author andrew
+ */
+public class dieRollTest {
+    
+    public dieRollTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of rollResult method, of class dieRoll.
+     */
+    @Test
+    public void testRollResult() {
+        System.out.println("rollResult");
+        int value = 6;
+        dieRoll instance = new dieRoll();
+        int result = instance.rollResult(value);
+        assertTrue((result >0) && (result < 7));
+        // TODO review the generated test code and remove the default call to fail.
+    }
+    
+}
