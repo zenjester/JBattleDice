ff84bd445a1f727f7edcdcc93dd55b6414b6e41f
andrew <andrew@andrew-t430s>
2016-04-06 21:59:45 +0100

    numbers to letters

--- src/jbattledice/gameModule.java
+++ src/jbattledice/gameModule.java
@@ -15,11 +15,33 @@ public class gameModule {
         String enemyForce;
         String tempForce ="";
         for (int i = 0; i < 20; i++) {
-           tempForce=tempForce+currentVal.rollResult(6);
+           tempForce=tempForce+armyType(currentVal.rollResult(6));
         }
         char[] ar = tempForce.toCharArray(); //TODO seperate method?
         Arrays.sort(ar);
         enemyForce = String.valueOf(ar);
         return enemyForce;
+    }
+    public String armyType(int rollVal)
+    {
+        String retVal="";
+        switch (rollVal){
+            case 1: retVal="A";
+            break;
+            case 2: retVal="M";
+            break;
+            case 3: retVal="W";
+            break;
+            case 4: retVal="G";
+            break;
+            case 5: retVal="A";
+            break;
+            case 6: retVal="M";
+            break;
+            default : retVal="Q";
+            break;                    
+                    
+        }
+        return retVal;
     }
 }
