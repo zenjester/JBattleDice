333812214eae016f6ff88912ffcdac56470c62e1
andrew <andrew@advancedsearch.virginmedia.com>
2016-04-29 22:41:44 +0100

    Added Buttons

--- src/jbattledice/JBattleDice.java
+++ src/jbattledice/JBattleDice.java
@@ -3,7 +3,7 @@ package jbattledice;
 import javax.swing.*;
 import java.awt.*;
 
-//test comment
+
 public class JBattleDice {
     public final static boolean RIGHT_TO_LEFT = false;
 
@@ -24,16 +24,42 @@ public class JBattleDice {
         myForce.setBackground(Color.GRAY);
         myForce.setForeground(Color.WHITE);
         JLabel compForce = new JLabel(currentGame.genResult());
-        //test compForce colors
+        JLabel playerLbl = new JLabel("Player");
+        JLabel computerLbl = new JLabel("Computer");
+        JLabel msgLbl = new JLabel("Message Area");
+        JLabel rollAreaLbl = new JLabel("Roll Area");
+        JButton yesBttn = new JButton("Yes");
+        JButton noBttn = new JButton("No");
+        JButton quitBttn = new JButton("Quit");
+        JButton saveBttn = new JButton("Save");
+        JButton endTurnBttn = new JButton("End Turn");
+        JPanel controlPanel = new JPanel();
+        JPanel controlPanel1= new JPanel();
+        controlPanel.add(yesBttn);
+        controlPanel.add(noBttn);
+        controlPanel1.add(quitBttn);
+        controlPanel1.add(saveBttn);
+        controlPanel1.add(endTurnBttn);
+        
+        
+        
+//test compForce colors
         compForce.setOpaque(true);
         compForce.setBackground(Color.BLUE);
         compForce.setForeground(Color.CYAN);
-
+        
+        contentPane.add(playerLbl);
+        contentPane.add(computerLbl);
         contentPane.add(myForce);
         contentPane.add(compForce);
+        contentPane.add(msgLbl);
+        contentPane.add(rollAreaLbl);
         contentPane.add(new JLabel(lbl1Txt+currentVal.rollResult(4)));
         contentPane.add(new JTextField(currentGame.genResult()));
-        contentPane.add(new JLabel(lbl1Txt+currentVal.rollResult(10)));
+        contentPane.add(controlPanel);
+        contentPane.add(controlPanel1);
+        
+ //       contentPane.add(new JLabel(lbl1Txt+currentVal.rollResult(10)));
     }
 
     private static void createAndShowGUI() {
@@ -41,7 +67,7 @@ public class JBattleDice {
 
         JFrame frame = new JFrame("BattleDice");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setPreferredSize(new Dimension(400, 300));
+        frame.getContentPane().setPreferredSize(new Dimension(600, 400));
 
         //Set up the content pane and components in GridLayout
         addComponentsToPane(frame.getContentPane());
